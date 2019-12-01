46bcb86113abf1b376fa8085a58bc9e6bd0069b4
Robert Rapacz <bercik468@gmail.com>
2019-12-01 19:50:56 +0100

    upgdare

--- js/nav_script.js
+++ js/nav_script.js
@@ -5,27 +5,34 @@ document.addEventListener('DOMContentLoaded', function () {
     const iconClose = document.querySelector(".close");
     const ulNavigation = document.querySelector(".navi");
     const liNavigation = document.querySelector(".navi").children;
+    const out = document.querySelectorAll(".out");
 
     const mobile = window.matchMedia("(max-width: 1024px)");
     mobile.addListener(function (isMobile) {
         if (isMobile.matches) {
-            ulNavigation.classList.remove("show")
-            iconHamburger.classList.remove("hidden")
-            iconClose.classList.remove("show")
-        }
-    })
+            ulNavigation.classList.remove("show");
+            iconHamburger.classList.remove("hidden");
+            iconClose.classList.remove("show");
+        };
+    });
 
     btn.addEventListener("click", function () {
-        ulNavigation.classList.toggle("show")
-        iconHamburger.classList.toggle("hidden")
-        iconClose.classList.toggle("show")
+        ulNavigation.classList.toggle("show");
+        iconHamburger.classList.toggle("hidden");
+        iconClose.classList.toggle("show");
     })
     for (let i = 0; i < liNavigation.length; i++) {
         liNavigation[i].addEventListener("click", function () {
-            ulNavigation.classList.toggle("show")
-            iconHamburger.classList.toggle("hidden")
-            iconClose.classList.toggle("show")
+            ulNavigation.classList.toggle("show");
+            iconHamburger.classList.toggle("hidden");
+            iconClose.classList.toggle("show");
+        });
+    };
+    for (let i = 0; i < out.length; i++) {
+        out[i].addEventListener("click", function () {
+            ulNavigation.classList.remove("show");
+            iconHamburger.classList.remove("hidden");
+            iconClose.classList.remove("show");
         })
     };
-
 });
